23475248f386d712b8cf1832aba761bf8037975f
live-jh <livejh7129@gmail.com>
2020-12-26 18:15:46 +0900

    user, comment model 추가

--- instagram/models.py
+++ instagram/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     post_img = models.ImageField(blank=True, upload_to="instagram/post/%Y/%m%d")
     is_public = models.BooleanField(default=False, verbose_name="공개여부")
@@ -14,8 +16,15 @@ class Post(models.Model):
         # return f"Custom Post object ({self.id}-{self.message})"
         return self.message
 
-    class Meta: # 기본 정렬 
+    class Meta:  # 기본 정렬
         ordering = ['-id']
     # def message_length(self): #message length 추가
     #     return len(self.message)
     # message.short_description = '메세지 글자수'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # "instagram.Post"
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
