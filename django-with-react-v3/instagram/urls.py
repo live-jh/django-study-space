95061dfc83a70d000f01d7c7fc3bfaf1046da0df
live-jh <livejh7129@gmail.com>
2021-03-06 23:57:40 +0900

    ViewSet 추가

--- django-with-react-v3/instagram/urls.py
+++ django-with-react-v3/instagram/urls.py
@@ -2,12 +2,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from . import views
-from .views import PublicPostListAPIView
+# from .views import PublicPostListAPIView
 
 router = DefaultRouter()
-router.register('post', views.PostViewSet)  # 2개의 URL 생성
+router.register('post', views.PostViewSet)  # 2개의 URL 생성, 첫번째 인자는 http 요청시 prefix로 지정하는 것 등록 ex: 'post/'
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('public/', PublicPostListAPIView.as_view(), name="post_list")
+    # path('public/', PublicPostListAPIView.as_view(), name="post_list")
 ]
--- django-with-react-v3/instagram/views.py
+++ django-with-react-v3/instagram/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -16,24 +16,55 @@ from .models import Post
 #     serializer_class = PostSerializer
 
 # Class를 이용한 View
-class PublicPostListAPIView(APIView):
-    def get(self, request):
-        post = Post.objects.filter(is_public=True)
-        post = PostSerializer(post, many=True).data
-        return Response(post, status=200)
+# class PublicPostListAPIView(APIView):
+#     def get(self, request):
+#         post = Post.objects.filter(is_public=True)
+#         post = PostSerializer(post, many=True).data
+#         return Response(post, status=200)
+
 
 # 함수를 이용한 View
-@api_view(['GET'])
-def public_post_list(request):
-    post = Post.objects.filter(is_public=True)
-    post = PostSerializer(post, many=True).data
-    return Response(post, status=200)
+# @api_view(['GET'])
+# def public_post_list(request):
+#     post = Post.objects.filter(is_public=True)
+#     post = PostSerializer(post, many=True).data
+#     return Response(post, status=200)
 
 
 # 밑에 2개의 처리를 이 하나의 코드로 대체
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    # ViewSet에 새로운 EndPoint
+    @action(detail=False, methods=['GET'])
+    def public(self, request):
+        qs = self.queryset.filter(is_public=True)
+        data_set = self.get_serializer(qs, many=True).data
+        return Response(data_set)
+
+    @action(detail=True, methods=['PATCH'])
+    def set_public(self, request, pk):
+        instance = self.get_object()
+        instance.is_public = True
+        instance.save(update_fields=['is_public'])
+        data = self.get_serializer(instance).data
+        return Response(data)
+
+    # def list(self, request, *args, **kwargs):
+    #     pass
+    #
+    # def create(self, request, *args, **kwargs):
+    #     pass
+    #
+    # def update(self, request, *args, **kwargs):
+    #     pass
+    #
+    # def retrieve(self, request, *args, **kwargs):
+    #     pass
+    #
+    # def partial_update(self, request, *args, **kwargs):
+    #     pass
 
     def dispatch(self, request, *args, **kwargs):
         print(request.body)  # logger
